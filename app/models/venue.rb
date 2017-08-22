a2b91eb3f08088c31b7c02881ff8b9e3d2a65ee0
Raghu Betina <raghu@firstdraft.com>
2017-08-22 02:27:00 +0000

    Direct association between neighborhoods and venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,9 @@
 class Venue < ApplicationRecord
   # Direct associations
 
+  belongs_to :neighborhood,
+             :counter_cache => true
+
   has_many   :pool_tables,
              :dependent => :destroy
 
